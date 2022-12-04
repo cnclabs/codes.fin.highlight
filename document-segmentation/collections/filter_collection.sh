86210b583c1fd99a05e257523df04c98183a69da
JH <jhj@JHs-MacBook-Air.local>
2022-12-04 23:45:27 +0800

    init commit with final v1.0 dataset

--- document-segmentation/collections/filter_collection.sh
+++
@@ -1,4 +0,0 @@
-python filter_collection.py \
---collection /tmp2/cwlin/fintext-new/tmp-collections/all/collections.txt \
---output_cmp /tmp2/cwlin/fintext-new/tmp-collections/8y-item7/8y-item7-company-more_than_5_sentences.txt \
---output /tmp2/cwlin/fintext-new/tmp-collections/8y-item7/8y-item7-collections.txt
